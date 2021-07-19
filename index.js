2f3494e6a713afbf6d60e21110791fe22525d991
Yathendrareddy <yathendrareddy412000@gmail.com>
2021-07-19 18:20:41 +0530

    Id

---
+++ index.js
@@ -0,0 +1,45 @@
+/**
+ * Function to populate values in card from form input
+ */
+function generateCard() {
+    // Get value of Student name from form input 
+    const nameElement = document.getElementById("name");
+    const nameValue = nameElement.value; 
+    // Assign the value of student name to generated card
+    const cardNameElement = document.getElementById("cardName");
+    cardNameElement.innerHTML = nameValue;
+
+    // Get value of college name from form input 
+    const collegeNameValue = document.getElementById("collegeName").value;
+    // Assign the value of college name to generated card
+    document.getElementById("cardCollegeName").innerHTML = collegeNameValue;
+
+    // Get value of location name from form input 
+    const locationValue = document.getElementById("location").value;
+    // Assign the value of location name to generated card
+    document.getElementById("cardLocation").innerHTML = locationValue;
+    
+    // Display final generated card to user     
+    document.getElementById("collegeCard").style.display = "block";
+}
+
+
+
+const chooseFile = document.getElementById("choose-file");
+const imgPreview = document.getElementById("img-preview");
+
+chooseFile.addEventListener("change", function () {
+  getImgData();
+});
+
+function getImgData() {
+  const files = chooseFile.files[0];
+  if (files) {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(files);
+    fileReader.addEventListener("load", function () {
+      imgPreview.style.display = "block";
+      imgPreview.innerHTML = '<img src="' + this.result + '" />';
+    });    
+  }
+}
